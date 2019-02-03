64301947fcd73f81927005ecb7bcc0d8a9518f32
eraskina <levrun1@gmail.com>
2019-02-03 17:54:56 +0100

    another file

---
+++ src/com/company/chapter0/Main2.java
@@ -0,0 +1,11 @@
+package com.company.chapter0;
+
+/**
+ * Created by levrun on 03.02.2019.
+ */
+public class Main2 {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
