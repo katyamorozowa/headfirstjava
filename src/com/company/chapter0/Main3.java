92acc2e8cb86f0dff940e48c3262a7dcc4b581d3
katyamorozowa <ekaterina.morosova@gmail.com>
2019-02-03 18:07:19 +0100

    KATYA

---
+++ src/com/company/chapter0/Main3.java
@@ -0,0 +1,12 @@
+package com.company.chapter0;
+
+/**
+ * Created by levrun on 03.02.2019.
+ */
+public class Main3
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is test 3");
+    }
+}
